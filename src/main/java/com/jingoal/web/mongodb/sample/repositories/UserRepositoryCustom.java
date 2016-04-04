43a8f7550a75d2d2c6f163ed13c6d359b3cfdf15
chenbin <c1325791024@163.com>
2016-04-04 13:15:19 +0800

    init repository

---
+++ src/main/java/com/jingoal/web/mongodb/sample/repositories/UserRepositoryCustom.java
@@ -0,0 +1,9 @@
+package com.jingoal.web.mongodb.sample.repositories;
+
+import com.jingoal.web.mongodb.sample.model.User;
+
+public interface UserRepositoryCustom {
+	
+	public void someCustomMethod(User user);
+	
+}
