0bae95c6da73039f1a93a1d5111ef628536a520c
chenbin <c1325791024@163.com>
2016-04-04 13:39:11 +0800

    重新提交

---
+++ src/main/java/com/jingoal/web/mongodb/sample/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.jingoal.web.mongodb.sample.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jingoal.web.mongodb.sample.model.User;
+
+public interface UserRepository extends CrudRepository<User, Long>, UserRepositoryCustom {
+
+	// Declare query methods here
+	
+	
+}
---
+++ src/main/java/com/jingoal/web/mongodb/sample/repositories/impl/UserRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.jingoal.web.mongodb.sample.repositories.impl;
+
+import com.jingoal.web.mongodb.sample.model.User;
+import com.jingoal.web.mongodb.sample.repositories.UserRepositoryCustom;
+
+public class UserRepositoryImpl implements UserRepositoryCustom {
+
+	public void someCustomMethod(User user) {
+		// Your custom implementation
+	}
+	
+}
