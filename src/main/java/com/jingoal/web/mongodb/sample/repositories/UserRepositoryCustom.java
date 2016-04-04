c0ead00ded79237c4b257e5d06ae93308c76774c
chenbin <c1325791024@163.com>
2016-04-04 13:18:29 +0800

    Revert "init repository"
    This reverts commit 43a8f7550a75d2d2c6f163ed13c6d359b3cfdf15.

--- src/main/java/com/jingoal/web/mongodb/sample/repositories/UserRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.jingoal.web.mongodb.sample.repositories;
-
-import com.jingoal.web.mongodb.sample.model.User;
-
-public interface UserRepositoryCustom {
-	
-	public void someCustomMethod(User user);
-	
-}
